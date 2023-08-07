f194e45b8cda8789eff75ef65240a37d1d792b58
IsabelaESC <128037931+IsabelaESC@users.noreply.github.com>
2023-08-07 12:14:08 -0300

    Update dbexercicio11.sql

--- BD_30.05/dbexercicio11.sql
+++ BD_30.05/dbexercicio11.sql
@@ -110,3 +110,127 @@ Nasc date not null,
 Id int,
 foreign key (Id) references tbCliente (Id)
 );
+
+
+
+
+
+
+
+
+
+create database dbdistribuidora;
+use dbdistribuidora;
+
+create table tbFornecedor(
+Codigo int primary key auto_increment,
+CNPJ decimal (14,0) unique,
+Nome varchar (200) not null,
+Telefone decimal (11,0)
+);
+
+create table tbCompra(
+NotaFiscal int primary key,
+DataCompra date not null,
+ValorTotal decimal (8,2) not null,
+QtdTotal int not null,
+Codigo int,
+foreign key (Codigo) references tbFornecedor (Codigo)
+);
+
+create table tbProduto(
+CodigoBarras decimal (14,0) primary key,
+Nome varchar (200) not null,
+ValorUnitario decimal (8,2) not null,
+Qtd int
+);
+
+create table tbNota_fiscal(
+NF int primary key,
+TotalNota decimal (8,2) not null,
+DataEmissao date not null
+);
+
+create table tbBairro(
+BairroId int primary key auto_increment,
+Bairro varchar (200) not null
+);
+
+create table tbCidade(
+CidadeId int primary key auto_increment,
+Cidade varchar (200) not null
+);
+
+create table tbEstado(
+UFId int primary key auto_increment,
+UF char (2) not null
+);
+
+
+create table tbItemCompra(
+NotaFiscal int,
+foreign key (NotaFiscal) references tbCompra (NotaFiscal),
+CodigoBarras decimal (14,0),
+foreign key (CodigoBarras) references tbProduto (CodigoBarras),
+constraint tbItemCompra primary key (NotaFiscal,CodigoBarras),
+ValorItem decimal (8,2) not null,
+Qtd int not null
+);
+
+create table tbEndereco(
+lougradouro varchar (200) not null,
+CEP decimal (8,0) primary key,
+BairroId int not null,
+foreign key (BairroId) references tbBairro (BairroId),
+CidadeId int not null,
+foreign key (CidadeId) references tbCidade (CidadeId),
+UFId int not null,
+foreign key (UFId) references tbEstado (UFId)
+);
+
+create table tbCliente(
+Id int primary key auto_increment,
+NomeCli varchar(200) not null,
+NumEnd decimal (6,0) not null,
+CompEnd varchar (50),
+CEPCli decimal (8,0),
+foreign key (CEPCli) references tbEndereco (CEP)
+);
+
+create table tbVenda(
+NumeroVenda int primary key,
+DataVenda date not null,
+TotalVenda decimal (8,2) not null,
+Id_Cli int not null,
+foreign key (Id_Cli) references tbCliente (Id),
+NF int,
+foreign key (NF) references tbNota_fiscal (NF)
+);
+
+create table tbItem_Venda(
+NumeroVenda int,
+foreign key (NumeroVenda) references tbVenda (NumeroVenda),
+CodigoBarras decimal (14,0),
+foreign key (CodigoBarras) references tbProduto (CodigoBarras),
+constraint tbItem_Venda primary key (NumeroVenda,CodigoBarras),
+ValorItem decimal (8,2) not null,
+Qtd int not null
+);
+
+create table tbcliente_PJ(
+CNPJ decimal (14,0) primary key,
+IE decimal (11,0) unique,
+Id int,
+foreign key (Id) references tbCliente (Id)
+);
+
+create table tbcliente_PF(
+CPF decimal (11,0) primary key,
+RG decimal (9,0) not null,
+RG_Dig char (1) not null,
+Nasc date not null,
+Id int,
+foreign key (Id) references tbCliente (Id)
+);
+
+
