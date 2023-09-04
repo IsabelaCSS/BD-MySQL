31dd04d111d7b30aedabdac3c44773b4e198b6a8
IsabelaESC <128037931+IsabelaESC@users.noreply.github.com>
2023-09-03 23:28:59 -0300

    Update dbexercicio11.sql

--- BD_30.05/dbexercicio11.sql
+++ BD_30.05/dbexercicio11.sql
@@ -1,122 +1,3 @@
-create database dbdistribuidora;
-use dbdistribuidora;
-
-create table tbFornecedor(
-Codigo int primary key auto_increment,
-CNPJ decimal (14,0) unique,
-Nome varchar (200) not null,
-Telefone decimal (11,0)
-);
-
-create table tbProduto(
-CodigoBarras decimal (14,0) primary key,
-Nome varchar (200) not null,
-ValorUnitario decimal (8,2) not null,
-Qtd int
-);
-
-create table tbNota_fiscal(
-NF int primary key,
-TotalNota decimal (8,2) not null,
-DataEmissao date not null
-);
-
-create table tbBairro(
-BairroId int primary key auto_increment,
-Bairro varchar (200) not null
-);
-
-create table tbCidade(
-CidadeId int primary key auto_increment,
-Cidade varchar (200) not null
-);
-
-create table tbEstado(
-UFId int primary key auto_increment,
-UF char (2) not null
-);
-
-create table tbCompra(
-NotaFiscal int primary key,
-DataCompra date not null,
-ValorTotal decimal (8,2) not null,
-QtdTotal int not null,
-Codigo int not null,
-foreign key (Codigo) references tbFornecedor (Codigo)
-);
-
-create table tbItemCompra(
-Qtd int not null,
-ValorItem decimal (8,2) not null,
-NotaFiscal int,
-foreign key (NotaFiscal) references tbCompra (NotaFiscal),
-CodigoBarras decimal (14,0),
-foreign key (CodigoBarras) references tbProduto (CodigoBarras),
-constraint tbItemCompra primary key (NotaFiscal,CodigoBarras)
-);
-
-create table tbEndereco(
-lougradouro varchar (200) not null,
-BairroId int,
-foreign key (BairroId) references tbBairro (BairroId),
-Cidade int,
-foreign key (Cidade) references tbCidade (CidadeId),
-UF int,
-foreign key (UF) references tbEstado (UFId),
-CEP decimal (8,0) primary key
-);
-
-create table tbCliente(
-Id int primary key auto_increment,
-NomeCli varchar(200) not null,
-NumEnd smallint not null,
-CompEnd varchar (50),
-CEP decimal (8,0),
-foreign key (CEP) references tbEndereco (Cep)
-);
-
-create table tbVenda(
-NumeroVenda int primary key,
-DataVenda date,
-TotalVenda decimal (8,2) not null,
-NF int,
-foreign key (NF) references tbNota_fiscal (NF),
-Id_Cli int,
-foreign key (Id_Cli) references tbCliente (Id)
-);
-
-create table tbItem_Venda(
-Qtd int not null,
-ValorItem decimal (8,2) not null,
-CodigoBarras decimal (14,0),
-foreign key (CodigoBarras) references tbProduto (CodigoBarras),
-NumeroVenda int,
-foreign key (NumeroVenda) references tbVenda (NumeroVenda),
-constraint tbItem_Venda primary key (CodigoBarras,NumeroVenda)
-);
-
-create table tbcliente_PJ(
-CNPJ decimal (14,0) primary key,
-IE decimal (11,0) unique,
-Id int,
-foreign key (Id) references tbCliente (Id)
-);
-
-create table tbcliente_PF(
-CPF decimal (11,0) primary key,
-RG decimal (9,0) not null,
-RG_Dig char (1) not null,
-Nasc date not null,
-Id int,
-foreign key (Id) references tbCliente (Id)
-);
-
-
-
-
-
-
-
 
 
 create database dbdistribuidora;
@@ -234,7 +115,6 @@ foreign key (Id) references tbCliente (Id)
 );
 
 -- Exercicio DML --
-drop procedure spSelectFornecedor;
 
 Delimiter $$
 create procedure spSelectFornecedor(vNome varchar (200),vCNPJ decimal (14,0),vTelefone decimal (11,0))
@@ -261,8 +141,6 @@ insert into tbCidade (Cidade)
 					values(vCidade);
 END $$
 
-drop procedure spSelectCidade;
-
 call spSelectCidade('Rio de Janeiro');
 call spSelectCidade('São Carlos');
 call spSelectCidade('Campinas');
@@ -273,9 +151,6 @@ call spSelectCidade('lapa');
 call spSelectCidade('Ponta Grossa');
 
 select * from tbCidade;
-
-
-drop procedure spSelectEstado;
 
 Delimiter $$
 create procedure spSelectEstado(vUF char(2))
@@ -307,9 +182,105 @@ select * from tbBairro;
 /* Exercício 5 */
 
 Delimiter $$
-create procedure spSelectProduto(vProduto varchar(200))
+create procedure spSelectProduto(vCodigoBarras decimal (14,0), vNome varchar (200),vValorUnitario decimal (8,2),vQtd int)
 Begin 
-insert into tbProduto(Produto) values (vProduto);
+insert into tbProduto(CodigoBarras,Nome,ValorUnitario,Qtd) 
+					values (vCodigoBarras,vNome,vValorUnitario,vQtd);
 END $$
 
-call spSelect
+call spSelectProduto('12345678910111','Rei de Papel Mache','54.61','120');
+call spSelectProduto('12345678910112','Bolinha de Sabão','100.45','120');
+call spSelectProduto('12345678910113','Carro Bate','44.00','120');
+call spSelectProduto('12345678910114','Bola Furada','10.00','120');
+call spSelectProduto('12345678910115','Maçã Laranja','99.44','120');
+call spSelectProduto('12345678910116','Boneoc de Hitler','124.00','200');
+call spSelectProduto('12345678910117','Farinha de Suruói','50.00','200');
+call spSelectProduto('12345678910118','Zelador de Cemitério','24.50','100');
+
+/*
+DECLARE lougradouro varchar (200) FALSE;
+					        IF NOT EXISTS (SELECT 1 FROM tbEndereco WHERE lougradouro = 'Rua da Federal') THEN
+					INSERT INTO minha_tabela (minha_coluna) VALUES (duplicado_value);
+*/
+Delimiter $$
+create procedure spSelectEndereco(vlougradouro varchar (200), vBairro varchar (200),vCidade varchar (200),vUF varchar (200),vCEP decimal (8,0))
+Begin 
+insert into tbEndereco (lougradouro,BairroId,CidadeId,UFId,CEP)
+					values (vlougradouro,vBairro,vCidade,vUF,vCEP);
+         
+END $$
+
+select * from tbBairro;
+select * from tbCidade;
+call spSelectBairro('Lapa')
+call spSelectCidade('São Paulo');
+	call spSelectEndereco('Rua da Federal',5,9,1,'12345050');
+
+
+call spSelectEndereco('Av Brasil',5,3,1,'12345051');
+
+
+select * from tbBairro;
+call spSelectBairro('Consolação');
+	call spSelectEndereco('Rua Liberdade',6,9,1,'12345052');
+
+
+select * from tbBairro;
+call spSelectBairro('Penha');
+	call spSelectEndereco('Av Paulista',7,1,3,'12345053');
+
+
+call spSelectEndereco('Rua Ximbú',7,1,3,'12345054');
+
+
+call spSelectEndereco('Rua Riu XI',7,3,1,'12345055');
+
+
+select * from tbCidade;
+call spSelectCidade('Barra Mansa')
+	call spSelectEndereco('Rua Chocolate',1,10,3,'12345056');
+
+
+select * from tbBairro;
+call spSelectbairro('Barra Funda')
+	call spSelectEndereco('Rua Pão na Chapa',8,8,2,'12345057');
+    
+    select * from tbEndereco;
+/*
+create view tb_Endereco
+as select 
+	tbEndereco.lougradouro,
+	tbEndereco.CEP,
+    tbBairro.Bairro,
+    tbCidade.Cidade,
+    tbEstado.UF 
+    from tbEndereco inner join tbBairro
+		on tbEndereco.BairroId = tbBairro.Bairro
+	inner join tbCidade
+		on tbEndereco.CidadeId = tbCidade.Cidade
+	inner join tbEstado
+		on tbEndereco.UFId = tbEstado.UF;
+        
+    select * from tb_Endereco;
+    
+    select lougradouro,Bairro,Cidade,UF,CEP from tb_Endereco;
+*/
+
+
+Delimiter $$
+create procedure spSelectPF(vId int, vNomeCli varchar(200),vCidade varchar (200),vUF varchar (200),vCEP decimal (8,0))
+Begin 
+insert into tbEndereco (lougradouro,BairroId,CidadeId,UFId,CEP)
+					values (vlougradouro,vBairro,vCidade,vUF,vCEP);
+         
+END $$
+
+
+        create table tbcliente_PF(
+CPF decimal (11,0) primary key,
+RG decimal (9,0) not null,
+RG_Dig char (1) not null,
+Nasc date not null,
+Id int,
+foreign key (Id) references tbCliente (Id)
+);
